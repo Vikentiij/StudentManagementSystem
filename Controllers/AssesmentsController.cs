976c4ab565abfb85e6a818ec331306ffa094a14c
Viktoriia Protopopova <vikentij.dean@gmail.com>
2022-06-06 21:34:13 +1200

    Add Assessment and StudentAssessment

---
+++ Controllers/AssesmentsController.cs
@@ -0,0 +1,167 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Data;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class AssesmentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AssesmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Assesments
+        [Authorize(Roles = "Admin,Teacher,Student")]
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Assesment.Include(a => a.Course);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Assesments/Details/5
+        [Authorize(Roles = "Admin,Teacher,Student")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assesment = await _context.Assesment
+                .Include(a => a.Course)
+                .FirstOrDefaultAsync(m => m.AssesmentId == id);
+            if (assesment == null)
+            {
+                return NotFound();
+            }
+
+            return View(assesment);
+        }
+
+        // GET: Assesments/Create
+        [Authorize(Roles = "Admin,Teacher")]
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "Name");
+            return View();
+        }
+
+        // POST: Assesments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin,Teacher")]
+        public async Task<IActionResult> Create([Bind("AssesmentId,CourseId,Title,DueDate")] Assesment assesment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(assesment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "Name", assesment.CourseId);
+            return View(assesment);
+        }
+
+        // GET: Assesments/Edit/5
+        [Authorize(Roles = "Admin,Teacher")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assesment = await _context.Assesment.FindAsync(id);
+            if (assesment == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "Name", assesment.CourseId);
+            return View(assesment);
+        }
+
+        // POST: Assesments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin,Teacher")]
+        public async Task<IActionResult> Edit(int id, [Bind("AssesmentId,CourseId,Title,DueDate")] Assesment assesment)
+        {
+            if (id != assesment.AssesmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(assesment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AssesmentExists(assesment.AssesmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "Name", assesment.CourseId);
+            return View(assesment);
+        }
+
+        // GET: Assesments/Delete/5
+        [Authorize(Roles = "Admin,Teacher")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assesment = await _context.Assesment
+                .Include(a => a.Course)
+                .FirstOrDefaultAsync(m => m.AssesmentId == id);
+            if (assesment == null)
+            {
+                return NotFound();
+            }
+
+            return View(assesment);
+        }
+
+        // POST: Assesments/Delete/5
+        [Authorize(Roles = "Admin,Teacher")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var assesment = await _context.Assesment.FindAsync(id);
+            _context.Assesment.Remove(assesment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AssesmentExists(int id)
+        {
+            return _context.Assesment.Any(e => e.AssesmentId == id);
+        }
+    }
+}
---
+++ Controllers/StudentAssesmentsController.cs
@@ -0,0 +1,173 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Data;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentAssesmentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentAssesmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: StudentAssesments
+        [Authorize(Roles = "Admin,Teacher,Student")]
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.StudentAssesment.Include(s => s.Assesment).Include(s => s.Student.UserData);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: StudentAssesments/Details/5
+        [Authorize(Roles = "Admin,Teacher,Student")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentAssesment = await _context.StudentAssesment
+                .Include(s => s.Assesment)
+                .Include(s => s.Student)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (studentAssesment == null)
+            {
+                return NotFound();
+            }
+
+            return View(studentAssesment);
+        }
+
+        // GET: StudentAssesments/Create
+        [Authorize(Roles = "Admin,Teacher")]
+        public IActionResult Create()
+        {
+            ViewData["AssesmentId"] = new SelectList(_context.Assesment, "AssesmentId", "Title");
+            ViewData["StudentId"] = new SelectList(_context.Students.Include(s => s.UserData), "Id", "FullName");
+            return View();
+        }
+
+        // POST: StudentAssesments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Admin,Teacher")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,StudentId,AssesmentId,Mark")] StudentAssesment studentAssesment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(studentAssesment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AssesmentId"] = new SelectList(_context.Assesment, "AssesmentId", "Title", studentAssesment.AssesmentId);
+            ViewData["StudentId"] = new SelectList(_context.Students.Include(s => s.UserData), "Id", "FullName", studentAssesment.StudentId);
+            return View(studentAssesment);
+        }
+
+        // GET: StudentAssesments/Edit/5
+        [Authorize(Roles = "Admin,Teacher")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentAssesment = await _context.StudentAssesment.FindAsync(id);
+            if (studentAssesment == null)
+            {
+                return NotFound();
+            }
+            ViewData["AssesmentId"] = new SelectList(_context.Assesment, "AssesmentId", "Title", studentAssesment.AssesmentId);
+            ViewData["StudentId"] = new SelectList(_context.Students.Include(s => s.UserData), "Id", "FullName", studentAssesment.StudentId);
+            return View(studentAssesment);
+        }
+
+        // POST: StudentAssesments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Admin,Teacher")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,StudentId,AssesmentId,Mark")] StudentAssesment studentAssesment)
+        {
+            if (id != studentAssesment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(studentAssesment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentAssesmentExists(studentAssesment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AssesmentId"] = new SelectList(_context.Assesment, "AssesmentId", "Title", studentAssesment.AssesmentId);
+            ViewData["StudentId"] = new SelectList(_context.Students.Include(s => s.UserData), "Id", "FullName", studentAssesment.StudentId);
+            return View(studentAssesment);
+        }
+
+        // GET: StudentAssesments/Delete/5
+        [Authorize(Roles = "Admin,Teacher")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentAssesment = await _context.StudentAssesment
+                .Include(s => s.Assesment)
+                .Include(s => s.Student)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (studentAssesment == null)
+            {
+                return NotFound();
+            }
+
+            return View(studentAssesment);
+        }
+
+        // POST: StudentAssesments/Delete/5
+        [Authorize(Roles = "Admin,Teacher")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var studentAssesment = await _context.StudentAssesment.FindAsync(id);
+            _context.StudentAssesment.Remove(studentAssesment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentAssesmentExists(int id)
+        {
+            return _context.StudentAssesment.Any(e => e.Id == id);
+        }
+    }
+}
