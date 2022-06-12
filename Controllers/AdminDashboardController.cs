80aedf777819189a3445fd45861486025859dd2b
Viktoriia Protopopova <vikentij.dean@gmail.com>
2022-06-12 13:00:46 +1200

    Fix Admin Dashboard pie chart

--- Controllers/AdminDashboardController.cs
+++ Controllers/AdminDashboardController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.Areas.Identity.Data;
 using StudentManagementSystem.Data;
 using StudentManagementSystem.ViewModels;
 using System.Linq;
@@ -10,23 +12,28 @@ namespace StudentManagementSystem.Controllers
     public class AdminDashboardController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdminDashboardController(ApplicationDbContext context)
+        public AdminDashboardController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
-            //var adminsCount = _context.Users.Where(u => u.Role == Models.Role.Admin).Count();
             var teachersCount = _context.Teachers.Count();
             var studentsCount = _context.Students.Count();
+
+            // To not deal with Identity Framework roles, get the adming count by sybsctracting teachers and students from total user count
+            var adminsCount = _userManager.Users.Count() - teachersCount - studentsCount;
+
             var coursesCount = _context.Courses.Count();
             var roomsCount = _context.Room.Count();
 
             var model = new AdminDashboardViewModel()
             {
-                AdminsCount = 1,
+                AdminsCount = adminsCount,
                 TeachersCount = teachersCount,
                 StudentsCount = studentsCount,
                 CoursesCount = coursesCount,
